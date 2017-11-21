ad99110b12b966b68162d524ed99e78f6390cfa3
abigailao <abigail.ao@outlook.com>
2017-11-21 14:04:49 +0100

    Wiskunde spel af!!!

--- WoordGame/woordGame.js
+++ WoordGame/woordGame.js
@@ -64,9 +64,8 @@ button.addEventListener("click", function(e){
 function typing(e) {
     typed = String.fromCharCode(e.which);
     for (var i = 0; i < spans.length; i++) {
-        if (spans[i].innerHTML === typed) { // if typed letter is the one from the word
-            if (spans[i].classList.contains("bg")) { // if it already has class with the bacground color then check the next one
-                // continue;
+        if (spans[i].innerHTML === typed) { // check of letter in het woord zit
+            if (spans[i].classList.contains("bg")) {
             } else if (spans[i].classList.contains("bg") === false && spans[i-1] === undefined || spans[i-1].classList.contains("bg") !== false ) { // if it dont have class, if it is not first letter or if the letter before it dont have class (this is done to avoid marking the letters who are not in order for being checked, for example if you have two "A"s so to avoid marking both of them if the first one is at the index 0 and second at index 5 for example)
                 spans[i].classList.add("bg");
                 break;
@@ -74,16 +73,11 @@ function typing(e) {
         }
     }
     var checker = 0;
-    for (var j = 0; j < spans.length; j++) { //checking if all the letters are typed
+    for (var j = 0; j < spans.length; j++) { //Checkt of alle letters getypt zijn
         if (spans[j].className === "span bg") {
             checker++;
         }
-        if (checker === spans.length) { // if so, animate the words with animate.css class
-            // spark.pause();
-            // spark.currentTime = 0;
-            // spark.play();
-            words.classList.add("animated");
-            words.classList.add("fadeOut");
+        if (checker === spans.length) {
             points++; //verhoogt de punten
             scoreDiv.innerHTML = points; //voegt punten aan de punten div toe
             document.removeEventListener("keydown", typing, false);
